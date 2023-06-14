64365ab7ffe1c6b62a127643bcfb5fb716bc2b60
tienbui <tienbui1508@gmail.com>
2023-06-15 00:38:26 +0930

    Initial Commit

---
+++ ResellTrack/ResellTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResellTrackApp.swift
+//  ResellTrack
+//
+//  Created by Tien Bui on 15/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct ResellTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
